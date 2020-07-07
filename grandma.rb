7fed4fd6386eb9fbd14447387354d04590bcf353
Clayton Moore <cmoorefour@gmail.com>
2020-07-07 16:37:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(string)
-if string == string
+if string.upcase == string
    "I LOVE YOU TOO PUMPKIN!"
-elsif string != string.upcase
+elsif string != string
    ("HUH? SPEAK UP, SONNY!")
 else ("NO, NOT SINCE 1938!")
  end
