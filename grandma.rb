a5307762ce0e74beac8357edefe77db5ec0c4926
Clayton Moore <cmoorefour@gmail.com>
2020-07-07 16:42:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
 if phrase.upcase == phrase
-   ("I LOVE YOU TOO PUMPKIN!")
+   "I LOVE YOU TOO PUMPKIN!"
 elsif phrase != phrase
-   ("HUH? SPEAK UP, SONNY!")
-else ("NO, NOT SINCE 1938!")
+   "HUH? SPEAK UP, SONNY!"
+else "NO, NOT SINCE 1938!"
 
  end
 end
